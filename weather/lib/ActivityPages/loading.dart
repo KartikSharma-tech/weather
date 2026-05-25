d68beb9a7eae26975e305fd71f11744730a2a68e
sharmakartik86000-code <sharmakartik86000@gmail.com>
2026-05-25 21:18:08 +0530

    pakage changes

--- weather/lib/ActivityPages/loading.dart
+++ weather/lib/ActivityPages/loading.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,34 +9,51 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
-
-
-
-    void timer(){
-      // process - 5 second 
-      print("Alarm Is Not  Ringing ");
-    }
-
+  void timer() {
+    // process - 5 second
+    print("Alarm Is Not  Ringing ");
+  }
 
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
-
       // appBar: AppBar(
       //   title: Text("hello"),
       // ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              "lib/ActivityPages/assets/images/AppIconImage.jpg",
+              width: 120,
+              height: 200,
+            ),
 
-     body:  Center(
-      child: Column(
-        children: <Widget>[
-          Text("Mousam App"),
-          Text("Made By Kartik Sharma")
-        ],
+            Text(
+              "Mousam App",
+              style: TextStyle(
+                fontSize: 40,
+                fontWeight: FontWeight.w500,
+                color: const Color.fromARGB(255, 245, 243, 244),
+              ),
+            ),
+            SizedBox(height: 15),
+            Text(
+              "Made By Kartik Sharma",
+              style: TextStyle(
+                fontSize: 17,
+                fontWeight: FontWeight.w400,
+                color: const Color.fromARGB(255, 245, 243, 244),
+              ),
+            ),
+
+            SizedBox(height: 25),
+            SpinKitWave(color: Colors.pink, size: 50.0),
+          ],
+        ),
       ),
-     ),
+      backgroundColor: Colors.blue[200],
     );
-
   }
-
 }
--- weather/lib/main.dart
+++ weather/lib/main.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:weather/ActivityPages/home.dart';
 import 'package:weather/ActivityPages/loading.dart';
-import 'package:weather/ActivityPages/location.dart'; 
 
 
 
