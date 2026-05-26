715a3b58c096255f74df5652a1eba4d0beaefd40
sharmakartik86000-code <sharmakartik86000@gmail.com>
2026-05-26 13:23:34 +0530

    Added loading screen navigation,Fixed weather card UI crash

--- weather/lib/ActivityPages/home.dart
+++ weather/lib/ActivityPages/home.dart
@@ -44,88 +44,168 @@ class _HomeState extends State<Home> {
     final random = Random();
     var city = city_name[random.nextInt(city_name.length)];
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text("Weather App"),
+
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                const Color.fromARGB(255, 242, 163, 6),
+                const Color.fromARGB(255, 234, 230, 230),
+                const Color.fromARGB(255, 54, 239, 3),
+              ],
+            ),
+          ),
+        ),
+      ),
+
       body: SafeArea(
-        child: Container(
-          color: Colors.blueGrey,
-          child: Column(
-            children: [
-              Container(                // Search Wala Container
+        child: SingleChildScrollView(
+          child: Container(
+            color: const Color.fromARGB(255, 101, 186, 229),
+            child: Column(
+              children: [
+                Container(
+                  // Search Wala Container
+                  padding: EdgeInsets.symmetric(horizontal: 10),
+                  margin: EdgeInsets.symmetric(horizontal: 22, vertical: 20),
+                  decoration: BoxDecoration(
+                    color: Colors.white,
 
-                padding: EdgeInsets.symmetric(horizontal: 10),
-                margin: EdgeInsets.symmetric(horizontal: 22, vertical: 20),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-
-                  borderRadius: BorderRadius.circular(14),
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 6,
-                      offset: Offset(0, 3),
-                    ),
-                  ],
-                ),
-                child: Row(
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        print("Search Me");
-                      },
-                      child: Container(
-                        child: Icon(Icons.search, color: Colors.blue),
-                        margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
+                    borderRadius: BorderRadius.circular(14),
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.black12,
+                        blurRadius: 6,
+                        offset: Offset(0, 3),
                       ),
-                    ),
-
-
-                    Expanded(
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Search  $city",
+                    ],
+                  ),
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          print("Search Me");
+                        },
+                        child: Container(
+                          child: Icon(Icons.search, color: Colors.blue),
+                          margin: EdgeInsets.fromLTRB(3, 0, 7, 0),
                         ),
                       ),
+
+                      Expanded(
+                        child: TextField(
+                          decoration: InputDecoration(
+                            border: InputBorder.none,
+                            hintText: "Search  $city",
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        margin: EdgeInsets.symmetric(horizontal: 25),
+                        padding: EdgeInsets.all(50),
+                        child: Row(
+                          children: [
+                            // Image.network("image insert"),
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text("Bad Weather....", style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold
+                                ),),
+                              Text("In Bhilwara",style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold
+                                ),)
+                              ],
+                            )
+                          ],
+                        ),
+
+                        // height: 100,
+                      ),
                     ),
                   ],
                 ),
-              ),
+                Row(
+                  children: [
+                    Expanded(
+                      child: Container(
+                        height: 300,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        margin: EdgeInsets.symmetric(
+                          horizontal: 25,
+                          vertical: 10,
+                        ),
+                        padding: EdgeInsets.all(50),
+                        child: Text("hh"),
 
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white.withOpacity(0.5)
+                        // height: 100,
                       ),
-                      margin: EdgeInsets.symmetric(horizontal: 25),
-                      padding: EdgeInsets.all(50),
-                      child: Text("hh"),
-                      // height: 100,
-                      
                     ),
+                  ],
+                ),
+
+                Row(
+                  // Ye Row H 2no box k liye sath vale 50-50
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        margin: EdgeInsets.fromLTRB(20, 0, 10, 0),
+                        padding: EdgeInsets.all(26),
+                        child: Text("hh"),
+                        height: 150,
+                      ),
+                    ),
+                    Expanded(
+                      child: Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: Colors.white.withOpacity(0.5),
+                        ),
+                        margin: EdgeInsets.fromLTRB(10, 0, 20, 0),
+                        padding: EdgeInsets.all(26),
+                        child: Text("h2"),
+                        height: 150,
+                      ),
+                    ),
+                  ],
+                ),
+                Container(
+                  padding: EdgeInsets.all(15),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      Text("Made By Kartik"),
+                      Text("Data Provided By Kartik's Weather App"),
+                    ],
                   ),
-                ],
-              ),
-              Row(
-                children: [
-                  Expanded(
-                    child: Container(
-                      height:300,
-                            decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(12),
-                              color: Colors.white.withOpacity(0.5)
-                            ),
-                            margin: EdgeInsets.symmetric(horizontal: 25,vertical: 10),
-                            padding: EdgeInsets.all(50),
-                            child: Text("hh"),
-                            // height: 100,
-                            
-                          ),
-                  ),
-                ],
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
       ),
--- weather/lib/ActivityPages/loading.dart
+++ weather/lib/ActivityPages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'dart:async';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
